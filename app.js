284f0ac2fd06ac8691bf08dbda2f05a8a3993244
Connor Ness <Connorrness@Gmail,com>
2018-04-08 21:28:48 +0100

    other repo

---
+++ app.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var url = ("mongodb://Setanta:1234@ds137019.mlab.com:37019/blogposts");
+
+var app = express();
+
+var mydb;
+
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, "/views"));
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  mydb = db.db('blogposts');
+  mydb.createCollection('posts', function(err, res) {
+    if (err) throw err;
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res){
+  mydb.blogposts.find(function(err, docs){
+    res.render('index', {
+      title: 'Blog Posts',
+      posts: docs
+    });
+  })
+});
+
+app.get('/blogposts', (req, res) => {
+  mydb.collection('posts').find().toArray(function(err, results){
+    res.send(results);
+  });
+});
+
+app.get('/blogposts/:id', (req, res) => {
+  mydb.collection('posts').findOne({_id: mongodb.ObjectID( req.params.id)}, (err, result) => {
+    res.send(result);
+  });
+});
+
+app.post('/blogposts', (req, res) => {
+  mydb.collection('posts').save(req.body, (err, result) => {
+    if (err) throw err;
+  });
+});
+
+app.put('/blogposts/update/:id', function(req,res){
+    const data = req.body;
+    db.posts.update({_id: ObjectId(req.params.id)}, {$set: data},function(err, result){
+        if(err) throw err;
+    });
+});
+
+app.delete('/blogposts/:id', (req, res) => {
+  mydb.collection('posts').deleteOne({_id: mongodb.ObjectID( req.params.id)}, (err, result) => {
+    if (err) throw err;
+  });
+});
+
+app.listen(3000);
---
+++ public/js/main.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+  $('.makePost').on('click', createPost);
+});
+
+$(document).ready(function(){
+  $('.postDelete').on('click', deletePost);
+});
+
+$(document).ready(function(){
+  $('.titleEdit').on('click', editTitle);
+});
+
+$(document).ready(function(){
+  $('.postEdit').on('click', editPost);
+});
+
+function createPost(){
+
+}
+
+function deletePost(){
+  var confirmation = confirm("Are you sure?");
+
+  if(confirmation){
+    $.ajax({
+      type: 'DELETE',
+      url: '/blogposts/delete/'+$(this).data().id
+    }).done(function(response){
+      window.location.replace('/');
+    });
+
+    window.location.replace('/');
+    } else{
+      return false;
+  }
+}
+
+function editTitle(){
+    var change = prompt('Change title to:', '');
+
+    while(change === '' || change === null){
+      return;
+    }
+    $.ajax({
+      type:'PUT',
+      url: '/blogposts/update/'+$(this).data().id,
+      data: {title: change}
+    }).done(function(response){
+      console.log(response);
+      window.location.replace('/editPost');
+    }).fail(function(response){
+      console.log("An Error Occured");
+    });
+}
+
+function editPost(){
+    var change = prompt('Change post:', '');
+
+    while(change === '' || change === null){
+      return;
+    }
+    $.ajax({
+      type:'PUT',
+      url: '/blogposts/update/'+$(this).data().id,
+      data: {title: change}
+    }).done(function(response){
+      console.log(response);
+      window.location.replace('/editPost');
+    }).fail(function(response){
+      console.log("An Error Occured");
+    });
+}
