d4de85acc2d96fff217d0bb75ca71756bb6f4386
Connor Ness <Connorrness@Gmail,com>
2018-04-09 11:31:09 +0100

    debug

--- app.js
+++ app.js
@@ -10,10 +10,14 @@ var app = express();
 var mydb;
 
 MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
+  if (err){
+    console.log('database connect error');
+  }
   mydb = db.db('blogposts');
   mydb.createCollection('posts', function(err, res) {
-    if (err) throw err;
+    if (err){
+      console.log('creation fail');
+    }
   });
 });
 
@@ -25,31 +29,43 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/blogposts', (req, res) => {
   mydb.collection('posts').find().toArray(function(err, results){
     res.send(results);
+    if (err){
+      console.log('adding to database fail');
+    }
   });
 });
 
 app.get('/blogposts/:id', (req, res) => {
   mydb.collection('posts').findOne({_id: mongodb.ObjectID( req.params.id)}, (err, result) => {
     res.send(result);
+    if (err){
+      console.log('find post fail');
+    }
   });
 });
 
 app.post('/blogposts', (req, res) => {
   mydb.collection('posts').save(req.body, (err, result) => {
-    if (err) throw err;
+    if (err){
+      console.log('save fail');
+    }
   });
 });
 
 app.put('/blogposts/update/:id', function(req,res){
     const data = req.body;
     db.posts.update({_id: ObjectId(req.params.id)}, {$set: data},function(err, result){
-        if(err) throw err;
+        if(err){
+          console.log('update fail');
+        }
     });
 });
 
 app.delete('/blogposts/:id', (req, res) => {
   mydb.collection('posts').deleteOne({_id: mongodb.ObjectID( req.params.id)}, (err, result) => {
-    if (err) throw err;
+    if (err){
+      console.log('delete fail');
+    }
   });
 });
 
